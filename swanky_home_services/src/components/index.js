07ca7c7feb786e242a196862784975a89ab89bfc
Matt E <mathew.estrada@sjsu.edu>
2024-06-02 22:41:50 -0700

    Base set-up for files

---
+++ swanky_home_services/src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import About from "./About";
+import Button from "./Button";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Reviews from "./Reviews";
+import Services from "./Services";
+
+export {
+    Navbar,
+    About, 
+    Button,
+    Contact,
+    Footer,
+    Hero,
+    Reviews,
+    Services
+};
